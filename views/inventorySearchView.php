da52a844a780285a2ccfa037204da45263ba030f
Jesse Tsang <JesseTsang@outlook.com>
2015-04-07 18:29:46 -0400

    Inserted a selection menu for product type.

--- views/inventorySearchView.php
+++ views/inventorySearchView.php
@@ -19,14 +19,29 @@ class InventorySearchView
 	
 	<body>
 		<div id="form" style="width:800px; margin:0 auto; padding-top: 100px;">
-			<form action=”#” method=”POST” name="historicalSearchForm" class="pure-form" id="inventorySearchForm">
+			<form action=”#” method=”POST” name="historicalSearchForm" class="pure-form pure-form-aligned" id="inventorySearchForm">
     			<fieldset>
         			<legend>Enter a product ID for a detailed product summary:</legend>
-
-        			<input id=”productID” name=”productIDField” type="text" placeholder="Enter Product ID"></br>
-
-					</br>
-        			<button type="submit" name=”submit” class="pure-button pure-button-primary">Submit</button>
+					
+					<div class="pure-control-group">
+						<label for="itemID">Product ID:</label>
+        				<input id=”productID” name=”productIDField” type="text" placeholder="Enter Product ID"></br>
+        			</div>	
+        			
+        			</br>
+        			<div class="pure-control-group">
+            			<label for="prodyctType">Product Type:</label>
+            			<select id="prodyctType">
+            				<option>Computer</option>
+            				<option>Parts</option>
+            				<option>Software</option>
+        				</select>
+        			</div>
+        			
+						</br>
+					<div class="pure-control-group">
+        				<button type="submit" name=”submit” class="pure-button pure-button-primary">Submit</button>
+        			</div>
     			</fieldset>
 			</form>
 		</div>
